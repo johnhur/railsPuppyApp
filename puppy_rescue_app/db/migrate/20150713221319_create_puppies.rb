8178916c7b3fb5bea98944ebf7654ca1319a9a7f
johnhur <johnjhur@gmail.com>
2015-07-13 15:13:58 -0700

    initial commit

---
+++ puppy_rescue_app/db/migrate/20150713221319_create_puppies.rb
@@ -0,0 +1,12 @@
+class CreatePuppies < ActiveRecord::Migration
+  def change
+    create_table :puppies do |t|
+      t.string :name
+      t.string :breed
+      t.text :bio
+      t.text :image_url
+
+      t.timestamps null: false
+    end
+  end
+end
